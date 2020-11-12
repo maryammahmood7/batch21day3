1ee65c2cdccf5d1037396952c6683d41939bba40
maryam <maryam.e.mahmood@gmail.com>
2020-11-12 10:07:16 -0600

    third commit

--- src/main.java
+++ src/main.java
@@ -2,5 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello last day git");
         //today is nice day
+        //its a sunny day
+        //yesterday was rainy
+
     }
 }
